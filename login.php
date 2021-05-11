e57302bfa84d0f0b0d8d375abec481fe1b20b0d2
Joao Vilar <joaovgiraldez@gmail.com>
2021-05-11 17:20:53 -0300

    Design da tela inicial completa (apenas css)

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <div class="container px-4 py-5 mx-auto">
+        <div class="card card0">
+            <div class="d-flex flex-lg-row flex-column-reverse">
+                <div class="card card1">
+                    <div class="row justify-content-center my-auto">
+                        <div class="col-md-8 col-10 my-5">
+                            <div class="row justify-content-center px-3 mb-3"> <img id="logo" src="alfredologo.jpg"> </div>
+                            <h3 class="mb-5 text-center heading">Alfredo</h3>
+                            <h4 class="msg-info">Entre com sua conta!</h4>
+                            <div class="form-group"> <label class="form-control-label text-muted">Usuário</label> <input type="text" id="email" name="email" placeholder="Email" class="form-control"> </div>
+                            <div class="form-group"> <label class="form-control-label text-muted">Senha</label> <input type="password" id="psw" name="psw" placeholder="Senha" class="form-control"> </div>
+                            <div class="row justify-content-center my-3 px-3"> <button class="btn-block btn-color">Alfreeedo!</button> </div>
+                            <div class="row justify-content-center my-2"> <a href="#"><small class="text-muted">Esqueceu sua senha?</small></a> </div>
+                        </div>
+                    </div>
+                    <div class="bottom text-center mb-5">
+                        <p href="#" class="sm-text mx-auto mb-3">Não possui uma conta?<button class="btn btn-white ml-2">Criar nova conta</button></p>
+                    </div>
+                </div>
+                <div class="card card2">
+                    <div class="my-auto mx-md-5 px-md-5 right">
+                        <h3 class="text-white">O Alfredo está aqui para você</h3> <small class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
