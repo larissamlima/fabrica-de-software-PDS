1b48021a4b16dc802c17ebb4d733612e45f93835
Joao Vilar <joaovgiraldez@gmail.com>
2021-05-12 21:34:49 -0300

    Concluído Login e Cadastro (Back e Front); Precisa alterar o caminho quando realiza o login pro dashboard

--- cadastro.php
+++ cadastro.php
@@ -1,45 +1,41 @@
 <?php
 
-$login = $_POST['login'];
 $senha = MD5($_POST['senha']);
 $email = $_POST['email'];
 $nome = $_POST['nome'];
 $sobrenome = $_POST['sobrenome'];
-$connect = mysqli_connect('localhost', 'admin', 'senha');
-$db = mysqli_select_db($con, $dbname);
-$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
-$select = mysqli_query($query_select, $connect);
-$array = mysqli_fetch_array($select);
-$logarray = $array['login'];
+echo $email;
+$connect = mysqli_connect('localhost', 'root', '');
+$db = mysqli_select_db($connect, 'alfredo');
 
-if ($mysqli->connect_errno) {
+if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
     exit();
+} else {
+    echo "success mothafocka";
 }
+// $query_select = "SELECT email FROM usuarios WHERE email = '$email'";
+// $select = mysqli_query($connect, $query_select);
+// $array = mysqli_fetch_array($select);
+// echo $array;
+// $emailarr = $array['email'];
 
-if ($login == "" || $login == null) {
+
+if ($email == "" || $email == null) {
     echo "<script language='javascript' type='text/javascript'>
     alert('O campo login deve ser preenchido');window.location.href='
     cadastro.html';</script>";
 } else {
-    if ($logarray == $login) {
+    $query = "INSERT INTO usuarios (senha,nome,sobrenome,email) VALUES ('$senha','$nome','$sobrenome','$email')";
+    $insert = mysqli_query($connect, $query);
 
+    if ($insert) {
         echo "<script language='javascript' type='text/javascript'>
-        alert('Esse login já existe');window.location.href='
-        cadastro.html';</script>";
-        die();
+      alert('Usuário cadastrado com sucesso!');window.location.
+      href='login.html'</script>";
     } else {
-        $query = "INSERT INTO usuarios (login,senha,nome,sobrenome,email) VALUES ('$login','$senha','$nome','$sobrenome','$email')";
-        $insert = mysqli_query($query, $connect);
-
-        if ($insert) {
-            echo "<script language='javascript' type='text/javascript'>
-          alert('Usuário cadastrado com sucesso!');window.location.
-          href='login.html'</script>";
-        } else {
-            echo "<script language='javascript' type='text/javascript'>
-          alert('Não foi possível cadastrar esse usuário');window.location
-          .href='cadastro.html'</script>";
-        }
+        echo "<script language='javascript' type='text/javascript'>
+      alert('Não foi possível cadastrar esse usuário');window.location
+      .href='cadastro.html'</script>";
     }
 }
--- login.php
+++ login.php
@@ -1,47 +1,20 @@
-<!DOCTYPE html>
+<?php
+$email = $_POST['email'];
+$entrar = $_POST['entrar'];
+$senha = md5($_POST['senha']);
+$connect = mysqli_connect('localhost', 'root', '');
+$db = mysqli_select_db($connect, 'alfredo');
+if (isset($entrar)) {
 
-
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-    <link rel="stylesheet" href="login.css">
-</head>
-
-<body>
-    <form>
-        <div class="container px-4 py-5 mx-auto">
-            <div class="card card0">
-                <div class="d-flex flex-lg-row flex-column-reverse">
-                    <div class="card card1">
-                        <div class="row justify-content-center my-auto">
-                            <div class="col-md-8 col-10 my-5">
-                                <div class="row justify-content-center px-3 mb-3"> <img id="logo" src="img/alfredologo.jpg"> </div>
-                                <h3 class="mb-5 text-center heading">Alfredo</h3>
-                                <h4 class="msg-info">Entre com sua conta!</h4>
-                                <div class="form-group"> <label class="form-control-label text-muted">Usuário</label> <input type="text" id="email" name="email" placeholder="Email" class="form-control"> </div>
-                                <div class="form-group"> <label class="form-control-label text-muted">Senha</label> <input type="password" id="psw" name="psw" placeholder="Senha" class="form-control"> </div>
-                                <div class="row justify-content-center my-3 px-3"> <button class="btn-block btn-color">Alfreeedo!</button> </div>
-                                <div class="row justify-content-center my-2"> <a href="#"><small class="text-muted">Esqueceu sua senha?</small></a> </div>
-                            </div>
-                        </div>
-                        <div class="bottom text-center mb-5">
-                            <p href="#" class="sm-text mx-auto mb-3">Não possui uma conta?<button class="btn btn-white ml-2"><a href="cadastro.php">Criar nova conta</a></button></p>
-                        </div>
-                    </div>
-                    <div class="card card2">
-                        <div class="my-auto mx-md-5 px-md-5 right">
-                            <h3 class="text-white">Chama o Alfredo!</h3> <small class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-</body>
-
-</html>
+    $verifica = mysqli_query($connect, "SELECT * FROM usuarios WHERE email =
+    '$email' AND senha = '$senha'") or die("erro ao selecionar");
+    if (mysqli_num_rows($verifica) <= 0) {
+        echo "<script language='javascript' type='text/javascript'>
+        alert('Email e/ou senha incorretos!');window.location
+        .href='login.html';</script>";
+        die();
+    } else {
+        setcookie("email", $email);
+        header("Location:index.php");
+    }
+}
