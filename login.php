d3d42eb9dc07345d35f3631d45a265d53ebfc2dc
Joao Vilar <joaovgiraldez@gmail.com>
2021-05-25 20:57:05 -0300

    adicionei o config.php e realizei a correção no login

--- login.php
+++ login.php
@@ -1,20 +1,21 @@
 <?php
 $email = $_POST['email'];
-#$entrar = $_POST['entrar'];
+$entrar = $_POST['entrar'];
 $senha = md5($_POST['senha']);
 $connect = mysqli_connect('localhost', 'root', '');
 $db = mysqli_select_db($connect, 'alfredo');
+
 if (isset($entrar)) {
 
     $verifica = mysqli_query($connect, "SELECT * FROM usuarios WHERE email =
     '$email' AND senha = '$senha'") or die("erro ao selecionar");
     if (mysqli_num_rows($verifica) <= 0) {
         echo "<script language='javascript' type='text/javascript'>
-        alert('Email e/ou senha incorretos!');window.location
+        alert('email e/ou senha incorretos');window.location
         .href='login.html';</script>";
         die();
     } else {
         setcookie("email", $email);
-        header("Location:index.php");
+        header("Location:index.html");
     }
 }
